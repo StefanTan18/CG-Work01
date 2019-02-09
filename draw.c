5fb7619b0f879eff1581550e9465ba59230f155b
Stefan Tan <stan3@stuy.edu>
2019-02-09 16:30:10 -0500

    Q3/Q4/Q7/Q8?

--- draw.c
+++ draw.c
@@ -7,26 +7,58 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
-  /*if (((y1 - y0)/(x1 - x0)) < 0) {
-    int temp = y1;
-    y1 = y0;
-    y0 = temp;
-    } Octants 3 and 4 */
-
-  /*if (x1 == x0) {
-    for (int y = y0; y <= ; y++) {
+  if (x1 == x0) {
+    for (int y = y0; y <= y1; y++) {
       plot (s, c, x0, y);
     }
-  } //doesn't work, when m = undefined */
-
+  }
+  
   else {
+    
+    if (x1 < x0 && y1 < y0) {
+      int temp0 = x1;
+      x1 = x0;
+      x0 = temp0;
+      int temp1 = y1;
+      y1 = y0;
+      y0 = temp1;
+    }
     
     int x = x0;
     int y = y0;
     int A = y1 - y0;
     int B = -(x1 - x0);
-    int m = -(A / B);
-    if (m < 1) {
+    int  m = -(A / B);
+    
+    if (m < -1) {
+      int d = 2*B - A;
+      
+      while (y >= y1) {
+	plot(s, c, x, y);
+	if (d < 0) {
+	  x++;
+	  d += 2*A;
+	}
+	y--;
+	d -= 2*B;
+      } 
+    }
+
+    else if (m < 0) {
+      int d = B - 2*A;
+      
+      while(x <= x1) {
+	plot(s, c, x, y);
+	if (d > 0) {
+	  y--;
+	  d -= 2*B;
+	}
+	x++;
+	d += 2*A;
+      }
+    }
+    
+    else if (m < 1) {
       int d = 2*A + B;
     
       while(x <= x1) {
