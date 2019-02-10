20c94b9925ff206ba481f4b26e576c9afddfbd2b
Stefan Tan <stan3@stuy.edu>
2019-02-09 23:29:44 -0500

    updated main.c & draw.c

--- draw.c
+++ draw.c
@@ -7,15 +7,7 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
-  if (x1 == x0) {
-    for (int y = y0; y <= y1; y++) {
-      plot (s, c, x0, y);
-    }
-  }
-  
-  else {
-    
-    if (x1 < x0 && y1 < y0) {
+  if (x1 < x0) {
       int temp0 = x1;
       x1 = x0;
       x0 = temp0;
@@ -23,38 +15,52 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
       y1 = y0;
       y0 = temp1;
     }
+  
+  if (x1 == x0) {
+    for (int y = y0; y <= y1; y++) {
+      plot (s, c, x0, y);
+    }
+  }
+
+  else if (y1 == y0) {
+    for (int x = x0; x <= x1; x++) {
+      plot (s, c, x, y1);
+    }
+  }
+  
+  else {
     
     int x = x0;
     int y = y0;
     int A = y1 - y0;
     int B = -(x1 - x0);
-    int  m = -(A / B);
+    double  m = -((double) A / B);
     
     if (m < -1) {
-      int d = 2*B - A;
+      int d = -2*B + A;
       
       while (y >= y1) {
 	plot(s, c, x, y);
-	if (d < 0) {
+	if (d >= 0) {
 	  x++;
-	  d -= 2*A;
+	  d += 2*A;
 	}
 	y--;
-	d += 2*B;
+	d += -2*B;
       } 
     }
 
     else if (m < 0) {
-      int d = B - 2*A;
+      int d = -2*A - B;
       
       while(x <= x1) {
 	plot(s, c, x, y);
-	if (d > 0) {
+	if (d <= 0) {
 	  y--;
-	  d += 2*B;
+	  d += -2*B;
 	}
 	x++;
-	d -= 2*A;
+	d += 2*A;
       }
     }
     
--- main.c
+++ main.c
@@ -18,17 +18,17 @@ int main() {
   clear_screen(s);
   
   draw_line(0, 0, 500, 500, s, c); //slope = 1 
-  draw_line(0, 500, 500, 0, s, c); //slope = -1 
-  draw_line(0, 250, 500, 250, s, c); //horizontal line 
-  draw_line(250, 0, 250, 500, s, c); //vertical line 
-  draw_line(0, 200, 500, 300, s, c); //Octant 1 
-  draw_line(0, 100, 150, 500, s, c); //Octant 2 
-  //draw_line(100, 500, 300, 0, s, c); //Octant 3
-  //draw_line(500, 0, 0, 100, s, c); //Octant 4 doesnt work
-  /* draw_line(500, 200, 0, 0, s, c); //Octant 5 */
-  /* draw_line(400, 500, 0, 0, s, c); //Octant 6 */
-  //draw_line(0, 400, 300, 0, s, c); //Octant 7 doesnt work
-  //draw_line(0, 300, 500, 200, s, c); //Octant 8 doesnt work
+  draw_line(0, 500, 500, 0, s, c); //slope = -1    
+  draw_line(0, 250, 500, 250, s, c); //horizontal line   
+  draw_line(250, 0, 250, 500, s, c); //vertical line    
+  draw_line(0, 200, 500, 300, s, c); //Octant 1   
+  draw_line(0, 100, 150, 500, s, c); //Octant 2   
+  draw_line(100, 500, 300, 0, s, c); //Octant 3 
+  draw_line(500, 0, 0, 100, s, c); //Octant 4  
+  draw_line(500, 200, 0, 0, s, c); //Octant 5  
+  draw_line(400, 500, 0, 0, s, c); //Octant 6  
+  draw_line(0, 400, 100, 0, s, c); //Octant 7 
+  draw_line(0, 400, 500, 350, s, c); //Octant 8  
   
   display(s);
   save_extension(s, "lines.png");
